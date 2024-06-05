83bd49468639f860d704e3b9aafdfc2875fbc055
nitesh <nitesh.khatri88@gmail.com>
2024-06-05 18:32:20 +0530

    updated

--- react-vite-zustard/react-vite-zustard-1/src/components/Navigation.jsx
+++ react-vite-zustard/react-vite-zustard-1/src/components/Navigation.jsx
@@ -4,28 +4,24 @@ import { Link, NavLink } from 'react-router-dom';
 
 const Navigation = () => {
 
-    const currentPage = useStore((state) => state.currentPage);
     const setCurrentPage = useStore((state) => state.setCurrentPage);
   
-    if (!currentPage) {
-      return null; // Render nothing if currentPage is undefined
-    }
   return (
     <>
      <nav>
       <ul>
         <li>
-          <NavLink  to="/" activeClassName="active" onClick={() => setCurrentPage('home')}>
+          <NavLink  to="/" activeclassname="active" onClick={() => setCurrentPage('home')}>
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink  to="/about" activeClassName="active" onClick={() => setCurrentPage('about')}>
+          <NavLink  to="/about" activeclassname="active" onClick={() => setCurrentPage('about')}>
             About
           </NavLink>
         </li>
         <li>
-          <NavLink to="/contact" activeClassName="active" onClick={() => setCurrentPage('contact')}>
+          <NavLink to="/contact" activeclassname="active" onClick={() => setCurrentPage('contact')}>
             Contact
           </NavLink>
         </li>
