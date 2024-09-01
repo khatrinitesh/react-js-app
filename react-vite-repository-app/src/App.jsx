8ba50adac4e62269772604f4c04167b3a9984287
niteshkhatri88 <nitesh.k@adsmn.in>
2024-09-01 16:27:58 +0530

    updated

--- react-vite-repository-app/src/App.jsx
+++ react-vite-repository-app/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import CustomApp from './repository/fatnav/2/CustomApp';
+import CustomApp from './repository/otp/1/CustomApp';
 function App() {
   return (
     <Router>
