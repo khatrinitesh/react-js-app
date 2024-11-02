02aff85d7b263dcfc4a5a419acaa581eae192a42
niteshkhatri88 <nitesh.k@adsmn.in>
2024-11-02 22:54:10 +0530

    updated

---
+++ react-js-mini-project-effort/react-vite-tailwindcss-mini-project-effort-15/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+// import ArrayExample from './Array/Array'
+// import AtComp from './Array/At'
+// import ConcatComp from './Array/Concat'
+// import CopyWithinComp from './Array/CopyWithin'
+// import EntriesComp from './Array/EntriesComp'
+// import EveryComp from './Array/EveryComp'
+import FillComp from './Array/FillComp'
+
+function App() {
+
+  return (
+    <>
+     <FillComp/>
+    </>
+  )
+}
+
+export default App
